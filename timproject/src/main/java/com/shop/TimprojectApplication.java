f503db90a69777c2d465fb7d590b035ae9917a39
Gion Go <gogion5@gmail.com>
2022-09-13 22:00:57 +0900

    Github Test

---
+++ timproject/src/main/java/com/shop/TimprojectApplication.java
@@ -0,0 +1,13 @@
+package com.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimprojectApplication.class, args);
+	}
+
+}
